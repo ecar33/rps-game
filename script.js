503c8264621460e11b787da32227632a3411e76f
ecar33 <evanc2022@gmail.com>
2024-01-10 14:32:02 -0600

    Add  ui and finish game logic

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function getComputerChoice() {
 
     switch (num) {
         case 0:
-            return "rock";
+            return rock;
         case 1:
-            return "paper";
+            return paper;
         case 2:
-            return "scissors";
+            return scissors;
     }
 }
 
@@ -17,63 +17,118 @@ function random(min, max) {
 
 
 function playRound(playerSelection, computerSelection) {
+
+    let playerScoreValue = Number(playerScore.textContent);
+    let computerScoreValue = Number(computerScore.textContent);
+    playerChoiceIcon.src = playerSelection.src;
+    computerChoiceIcon.src = computerSelection.src;
+
+    toggleActive(menuContainer);
+    toggleActive(roundContainer);
+
     if (playerSelection == computerSelection) {
-        console.log("It's a tie! Another game will be played.")
-        return 0;
-    } else if (playerSelection == "rock" && computerSelection == "scissors" ||
-        playerSelection == "paper" && computerSelection == "rock" ||
-        playerSelection == "scissors" && computerSelection == "paper") {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-        return 1;
+        outcomeIcon.src = "./imgs/draw.png"
+        return [playerScoreValue, computerScoreValue]
+
+    } else if (playerSelection == rock && computerSelection == scissors ||
+        playerSelection == paper && computerSelection == rock ||
+        playerSelection == scissors && computerSelection == paper) {
+
+        outcomeIcon.src = "./imgs/win.png"
+        playerScoreValue += 1;
+        playerScore.textContent = playerScoreValue;
+        return [playerScoreValue, computerScoreValue]
+
     } else {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
-        return 2;
+
+        outcomeIcon.src = "./imgs/lose.png"
+        computerScoreValue += 1;
+        computerScore.textContent = computerScoreValue;
+        return [playerScoreValue, computerScoreValue]
     }
 }
 
-function getPlayerSelection() {
-    let choice = prompt(`Select a move for this game: 
-    Rock = 0
-    Paper = 1
-    Scissors = 2`)
-
-    choice = parseInt(choice)
-    switch (choice) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
-        default:
-            console.log("Incorrect input, try again")
-            return 0;
+function toggleActive(element) {
+    classList = Array.from(element.classList)
+    if (classList.includes('active')) {
+        element.classList.add("inactive");
+        element.classList.remove("active");
+    } else {
+        element.classList.add("active");
+        element.classList.remove("inactive");
     }
 }
 
-let playerScore = 0;
-let computerScore = 0;
+function playGame(event) {
+    let computerSelection = getComputerChoice()
+    let playerSelection = event.target;
+    [playerScoreValue, computerScoreValue] = playRound(playerSelection, computerSelection);
+    checkAndDisplayWinner(playerScoreValue, computerScoreValue);
+}
 
-while (playerScore < 3 && computerScore < 3) {
-    let roundOutcome = 0;
-    console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`)
-    playerSelection = getPlayerSelection();
-    computerSelection = getComputerChoice();
-    roundOutcome = parseInt(playRound(playerSelection, computerSelection));
-    if (roundOutcome == 0) {
-        continue;
-    } else if (roundOutcome == 1) {
-        playerScore++;
-    }
-    else if (roundOutcome == 2) {
-        computerScore++;
+function checkAndDisplayWinner(playerScore, computerScore) {
+    if (playerScore === 5 || computerScore === 5) {
+        toggleActive(roundContainer)
+        toggleActive(winnerContainer)
+        if (playerScore === 5) {
+            winnerIcon.src = "./imgs/player.png"
+            winnerText.textContent = "Player wins! Congrats!"
+        }
+        else {
+            winnerIcon.src = "./imgs/computer.svg"
+            winnerText.textContent = "Computer wins! Oh well."
+        }
     }
 }
 
-if (playerScore > computerScore) {
-    console.log("Congratulations, you won!");
-} else {
-    console.log("Sorry, the computer won.");
-}
 
-console.log("Thanks for playing.");
+
+const rock = document.querySelector("[data-choice='rock']");
+const paper = document.querySelector("[data-choice='paper']");
+const scissors = document.querySelector("[data-choice='scissors']");
+
+const playerScore = document.querySelector("#player-score-value");
+const computerScore = document.querySelector("#computer-score-value");
+
+const roundContainer = document.querySelector(".round-container");
+const winnerContainer = document.querySelector(".winner-container")
+const menuContainer = document.querySelector(".menu-container");
+const menuButton = document.querySelector(".menu-button");
+const newGameButton = document.querySelector(".new-game-button");
+
+const playerChoiceIcon = document.querySelector(".player_choice_icon");
+const computerChoiceIcon = document.querySelector(".computer_choice_icon");
+const outcomeIcon = document.querySelector(".outcome_icon");
+const winnerIcon = document.querySelector(".winner-icon");
+const winnerText = document.querySelector(".winner-text")
+
+
+rock.addEventListener("click", function (event) {
+    playGame(event)
+})
+
+paper.addEventListener("click", function (event) {
+    playGame(event)
+})
+
+scissors.addEventListener("click", function (event) {
+    playGame(event)
+})
+
+menuButton.addEventListener("click", function () {
+    toggleActive(menuContainer);
+    toggleActive(roundContainer);
+})
+
+newGameButton.addEventListener("click", function () {
+    toggleActive(winnerContainer)
+    toggleActive(menuContainer)
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+
+})
+
+
+
+
+
